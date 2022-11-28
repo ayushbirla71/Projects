d7c55a3a8bfc3579d0730c27b5279d2cc27c5a3c
subho-Yadav <subhoyadav16@gmail.com>
2022-11-28 10:21:27 +0530

    create review without validations

--- src/router/route.js
+++ src/router/route.js
@@ -2,6 +2,7 @@ const express= require('express');
 const router= express.Router();
 const bookController=require('../controller/booksControler');
 const userController=require('../controller/userControler');
+const reviewControler=require("../controller/reviewController")
 const middelware=require('../midelware/auth')
 
 router.post('/register', userController.userCreate)
@@ -9,6 +10,7 @@ router.post('/login',userController.userLogin)
 router.get('/user',middelware.Authentication)
 router.post('/books', bookController.bookCreate)
 router.get('/books',bookController.getAllBooks)
+router.post("/books/:bookId/review",reviewControler.createReview)
 
 router.all('/*',function(req,res){
     return res.status(400).send({status:false, message:"pls provide valid path"})
