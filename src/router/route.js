1e4f36482a3202abf8aba28aafa2e67d665b3131
ayushbirla71 <ayushbirla71@gmail.com>
2022-11-28 21:05:31 +0530

    validation in book controler

--- src/router/route.js
+++ src/router/route.js
@@ -2,15 +2,18 @@ const express= require('express');
 const router= express.Router();
 const bookController=require('../controller/booksControler');
 const userController=require('../controller/userControler');
-const reviewControler=require("../controller/reviewController")
+const reviewControler=require('../controller/reviewController')
+const update =require("../controller/update")
+
 const middelware=require('../midelware/auth')
 
 router.post('/register', userController.userCreate)
 router.post('/login',userController.userLogin)
 router.get('/user',middelware.Authentication)
-router.post('/books',middelware.Authentication, bookController.bookCreate)
+router.post('/books',middelware.Authentication ,bookController.bookCreate)
 router.get('/books',middelware.Authentication,bookController.getAllBooks)
-router.post('/books/:bookId/review',reviewControler.createReview)
+router.post("/books/:bookId/review",reviewControler.createReview)
+router.put("/books/:bookId/review/:reviewId",update.updateReview)
 
 router.all('/*',function(req,res){
     return res.status(400).send({status:false, message:"pls provide valid path"})
