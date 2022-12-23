b8a551791cca4f0e2011202b969f2341abab171e
ayushbirla71 <ayushbirla71@gmail.com>
2022-12-23 11:00:20 +0530

    create cart validation

--- src/controllers/cartControllet.js
+++ src/controllers/cartControllet.js
@@ -1,11 +1,27 @@
 const cartModel = require('../models/cartModel')
 const productModel = require('../models/productModel')
+const userModel = require('../models/userModel')
+const { isValidObjectId } = require('../validators/validations')
 
 const createCart = async (req, res) => {
     try {
         let userId = req.params.userId
+        if (!isValidObjectId(userId)) return res.status(400).send({ status: false, message: "Pls provide valid userId" })
+        let userData = await userModel.findById(userId)
+        if (!userData) return res.status(404).send({ status: false, message: "user not found" })
         let { cartId, productId } = req.body
-        let { price } = await productModel.findById(productId)
+        if (!productId || !isValidObjectId(productId)) return res.status(400).send({ status: false, message: "Pls provide valid productId" })
+        let cartData = await cartModel.findOne({ userId: userId })
+        if (cartData) {
+            if (!cartId) return res.status(400).send({ status: false, message: "user cart exist pls provide cart id" })
+            if (cartId) {
+                if (!isValidObjectId(cartId)) return res.status(400).send({ status: false, message: "Pls provide valid cartId" })
+                if (cartData._id != cartId) return res.status(400).send({ status: false, message: "this cart id is ont mach" })
+            }
+        }
+        let productData = await productModel.findById(productId)
+        if (!productData) return res.status(404).send({ status: false, message: "Product not found" })
+        let { price } = productData
         let Obj = {}
         let CreateCart
         if (!cartId) {
@@ -16,7 +32,7 @@ const createCart = async (req, res) => {
         else {
             let cartData = await cartModel.findById(cartId).select({ items: 1, totalItems: 1, totalPrice: 1 })
             let xyz = cartData.items
-            let ab=true
+            let ab = true
             for (let i = 0; i < xyz.length; i++) {
                 let ele = xyz[i]
                 if (ele.productId == productId) {
